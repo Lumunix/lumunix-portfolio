0a714a039ce2f071d543ec4c77ebd7757515800c
Lumunix <lumunix@icloud.com>
2025-01-19 18:09:03 -0500

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,31 @@
                 <button class="btn btn-primary">Action</button>
             </footer>
         </div>
+        <a class="card card-hover overflow-hidden" href="/elements/cards">
+						<header>
+
+						</header>
+						<div class="p-4 space-y-4">
+							<h6 class="h6" data-toc-ignore>Announcements</h6>
+							<h3 class="h3" data-toc-ignore>Skeleton is Awesome!</h3>
+							<article>
+								<p>
+									<!-- cspell:disable -->
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet eligendi cumque consequatur tempore
+									sint nisi sapiente. Iste beatae laboriosam iure molestias cum expedita architecto itaque quae rem.
+									<!-- cspell:enable -->
+								</p>
+							</article>
+						</div>
+						<hr class="opacity-50" />
+						<footer class="p-4 flex justify-start items-center space-x-4">
+
+							<div class="flex-auto flex justify-between items-center">
+								<h6 class="font-bold" data-toc-ignore>By Alex</h6>
+								<small>On {new Date().toLocaleDateString()}</small>
+							</div>
+						</footer>
+					</a>
 
         <!-- Card 2 -->
         <div class="card space-y-2">
