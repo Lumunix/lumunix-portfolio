68b39c3671809a565f6fcb75c4f7c8e483215556
Lumunix <lumunix@icloud.com>
2025-01-19 16:04:02 -0500

    base

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+    import routes from '$lib/NavRoutes';
+</script>
+
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	Dylan Poljak
+	<svelte:fragment slot="trail">
+	<LightSwitch />
+	</svelte:fragment>
+
+</AppBar>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+    import { AppBar } from '@skeletonlabs/skeleton';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+
+
+
+
+</div>
+
+
